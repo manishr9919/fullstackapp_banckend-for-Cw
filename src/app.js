b1a78161e907b488952017e4c488053d473dc22f
manishr9919 <manishr9919@gamil.com>
2024-12-01 20:48:39 +0530

    3

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use("/signup", signupRote);
 app.use("/signin", loginRoute);
+app.get("/", (req, res) => {
+  res.send("ok");
+});
 
 app.listen(PORT, async () => {
   try {
