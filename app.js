3c59c13b7003b6b6b931e5555589a0f904488e1d
manishr9919 <manishr9919@gamil.com>
2024-12-02 13:23:52 +0530

    change

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const connect = require("./config/db");
-// const signupRote = require("./src/Route/user.signup.route");
-// const loginRoute = require("./src/Route/user.login.route");
+const signupRote = require("./src/Route/user.signup.route");
+const loginRoute = require("./src/Route/user.login.route");
+const registration = require("../fullstackapp_banckend-for-Cw/src/model/user.signUp.model");
 const app = express();
 app.use(express.json());
-// app.use("/signup", signupRote);
-// app.use("/signin", loginRoute);
+app.use("/signup", signupRote);
+app.use("/signin", loginRoute);
 app.get("/", (req, res) => {
   res.send("ok");
+});
+app.get("/getAllUser", async (req, res) => {
+  try {
+    const allUser = registration.find();
+    res.status(200).json({ message: " fetch all user is successful", allUser });
+  } catch (error) {}
 });
 
 app.listen(PORT, async () => {
